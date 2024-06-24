10780093c114bf05943924676c1005b0f80b2f2c
Aldo-garciayt <87112046+Aldo-garciayt@users.noreply.github.com>
2024-06-24 10:02:53 -0600

    apuntes 3

---
+++ EjercicioPE1/src/ejerciciope1/EjercicioPE1.java
@@ -0,0 +1,19 @@
+//Declar dos variables numericas y mostrar el reslutados entre ellas de una suma,resta,multiplicacion, division y modulo
+package ejerciciope1;
+public class EjercicioPE1 {
+
+    /*//
+     */
+    public static void main(String[] args) {
+        int n1=10;
+        int n2=20;
+        
+        System.out.println("La suma de los dos numeros es:" + (n1+n2));
+        System.out.println("La resta de los dos numeros es:" + (n1-n2));
+        System.out.println("La multiplicacion de los dos numeros es:" + (n1*n2));
+        System.out.println("La division de los dos numeros es:" + (n1/n2));
+        System.out.println("El modulo de los dos numeros es:" + (n1%n2));
+        
+    }
+    
+}
---
+++ EjercicioPE3/src/ejerciciope3/EjercicioPE3.java
@@ -0,0 +1,18 @@
+
+package ejerciciope3;
+
+import java.util.Scanner;
+
+public class EjercicioPE3 {
+
+    public static void main(String[] args) {
+    //String nombre;
+    
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Introducir nombre");
+    String nombre=sc.nextLine();
+    
+    System.out.println("Bienvenido" + nombre);
+    }
+    
+}
